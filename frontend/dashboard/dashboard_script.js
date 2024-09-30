b0f63ace8f856cb585476a799083d639b9360063
Belal Ahmed <belal.ahmd219@gmail.com>
2024-09-30 07:20:09 +0300

    added sidebar

--- frontend/dashboard/dashboard_script.js
+++ frontend/dashboard/dashboard_script.js
@@ -219,3 +219,14 @@ function newList(id,text,body,btn){
     body.appendChild(btn)
     return newList
 }
+
+function closeMenu(){
+    document.getElementById("sideBar").style.width = "0px";
+    document.getElementById("sideBar").style.padding = "0px";
+
+}
+
+function openMenu(){
+    document.getElementById("sideBar").style.width = "20vw"
+    document.getElementById("sideBar").style.padding = "10px";
+}
---
+++ frontend/navBar/sideBar_script.js
@@ -0,0 +1,10 @@
+function closeMenu(){
+    document.getElementById("sideBar").style.width = "0px";
+    document.getElementById("sideBar").style.padding = "0px";
+
+}
+
+function openMenu(){
+    document.getElementById("sideBar").style.width = "20vw"
+    document.getElementById("sideBar").style.padding = "10px";
+}
