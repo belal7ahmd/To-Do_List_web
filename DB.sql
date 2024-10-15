661b057fcbd8d4fc3c7d55ae74a3ca2c199c91de
Belal Ahmed <belal.ahmd219@gmail.com>
2024-10-15 07:42:51 +0300

    Fixed Database
    Fixed database

---
+++ DB.sql
@@ -0,0 +1,31 @@
+create schema todo_list;
+
+use todo_list;
+create table if not exists user_(
+user_id varchar(36) primary key,
+username varchar(30),
+user_email longtext,
+user_password longtext
+);
+
+create table if not exists list_(
+list_id varchar(36) primary key,
+list_title longtext,
+user_id varchar(36),
+time_ bigint,
+foreign key (user_id) references user_(user_id) on delete cascade
+);
+
+create table if not exists todo_(
+todo_id varchar(36) primary key,
+todo_title longtext,
+list_id varchar(36),
+time_ bigint,
+foreign key (list_id) references list_(list_id) on delete cascade
+);
+use todo_list;
+drop user program@localhost;
+flush privileges;
+create user program@localhost identified by 'pms2013pms';
+
+GRANT ALL ON *.* TO 'program'@'localhost' WITH GRANT OPTION;
